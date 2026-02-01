bc95a6f2585c3faad52de1b5900e35ee21c38d22
Arnav1403 <ajgamerz835@gmail.com>
2026-02-01 21:31:03 +0530

    Insert an Element in an Array

---
+++ Q1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, pos, x;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the position to insert (1 to %d): ", n + 1);
+    scanf("%d", &pos);
+
+    printf("Enter the element to insert: ");
+    scanf("%d", &x);
+
+    for (i = n - 1; i >= pos - 1; i--) {
+        arr[i + 1] = arr[i];
+    }
+
+    arr[pos - 1] = x;
+    n++;
+
+    printf("Array after insertion:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
