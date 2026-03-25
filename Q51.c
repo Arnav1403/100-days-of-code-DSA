a33c61073a89576d08852623412321c43d87b23b
Arnav1403 <ajgamerz835@gmail.com>
2026-03-25 22:03:42 +0530

    Lowest Common Ancestor in BST

---
+++ Q51.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+
+struct Node* newNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+
+struct Node* insert(struct Node* root, int key) {
+    if (root == NULL)
+        return newNode(key);
+
+    if (key < root->data)
+        root->left = insert(root->left, key);
+    else
+        root->right = insert(root->right, key);
+
+    return root;
+}
+
+
+struct Node* findLCA(struct Node* root, int n1, int n2) {
+    while (root != NULL) {
+        if (n1 < root->data && n2 < root->data)
+            root = root->left;
+        else if (n1 > root->data && n2 > root->data)
+            root = root->right;
+        else
+            return root;
+    }
+    return NULL;
+}
+
+int main() {
+    int n, i, val, n1, n2;
+    struct Node* root = NULL;
+
+    
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &val);
+        root = insert(root, val);
+    }
+
+    scanf("%d %d", &n1, &n2);
+
+    
+    struct Node* lca = findLCA(root, n1, n2);
+    if (lca != NULL)
+        printf("%d\n", lca->data);
+
+    return 0;
+}
