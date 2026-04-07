3b03ee8c5365d911439100a82ff9f5eb3068e753
Arnav1403 <ajgamerz835@gmail.com>
2026-04-07 21:10:13 +0530

    Graph Representation Using Adjacency List

---
+++ Q62.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    
+    struct Node* adj[n];
+
+    
+    for (int i = 0; i < n; i++) {
+        adj[i] = NULL;
+    }
+
+    
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+
+        
+        struct Node* newNode = createNode(v);
+        newNode->next = adj[u];
+        adj[u] = newNode;
+
+      
+    }
+
+   
+    for (int i = 0; i < n; i++) {
+        printf("%d -> ", i);
+        struct Node* temp = adj[i];
+        while (temp != NULL) {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
